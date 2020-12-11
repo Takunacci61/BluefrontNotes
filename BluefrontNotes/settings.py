ff3115a60bab87a7ae8736a91b4b3c2f806e2a45
Takudzwa <ngagwetasimba@gmail.com>
2020-12-11 00:45:19 +0000

    updatr
    updatr

--- BluefrontNotes/settings.py
+++ BluefrontNotes/settings.py
@@ -165,7 +165,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'bluefrontdev'
+EMAIL_HOST_USER = 'bluefrontdev@gmail.com'
 EMAIL_HOST_PASSWORD = 'takutakudzwa'
 django_heroku.settings(locals())
 
